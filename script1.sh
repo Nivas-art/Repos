aec09be0d38f156f02a4ee659478c850799c8c90
Nivas-art <srinujunnu123@gmail.com>
2024-07-20 22:23:55 +0530

    practice

--- script1.sh
+++ script1.sh
@@ -1,7 +1,21 @@
 #!bin/bash
 
 USERID=$(id -u)
-
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.....$R FAILURE $N"
+        exit 1
+    elase
+        echo "$2.....$G SUCCES $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then 
@@ -11,18 +25,9 @@ else
     echo "your in root user"
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2.....FAILURE"
-        exit 1
-    elase
-        echo "$2.....SUCCES"
-    fi
-}
 
-dnf install mysql -y 
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing of mysql"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing of git"
